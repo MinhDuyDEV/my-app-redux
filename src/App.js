2fba9fbb08693a345f94bddfa4b4859d7dc858cb
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-01-27 22:56:35 +0700

    handle dark mode, UI toggle sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import "./App.css";
-import Counter from "./components/Counter";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import useDarkMode from "./hooks/useDarkMode";
+import { toggleDarkMode } from "./redux-toolkit/globalSlice";
+import Card from "./uis/Card";
 
 function App() {
-  const { count } = useSelector((state) => state.counter);
+  const globalOptions = useSelector((state) => state.global);
+  console.log("🚀 ~ file: App.js:9 ~ App ~ globalOptions", globalOptions);
+  const dispatch = useDispatch();
+  // const handleToggleDarkMode = () => {
+  //   dispatch(toggleDarkMode());
+  const [darkMode, setDarkMode] = useDarkMode();
+  // };
+  useEffect(() => {
+    dispatch(toggleDarkMode(darkMode));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  // console.log("🚀 ~ file: App.js:14 ~ App ~ darkMode", darkMode);
+  const handleToggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    dispatch(toggleDarkMode(!darkMode));
+  };
   return (
     <div>
-      <h2>The counter from app: {count}</h2>
-      <Counter></Counter>
+      <Card></Card>
+      <button
+        onClick={handleToggleDarkMode}
+        className="p-3 mt-10 bg-blue-500 rounded-lg"
+      >
+        Toggle dark mode
+      </button>
     </div>
   );
 }
