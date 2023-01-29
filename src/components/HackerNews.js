6d6c24e2d496926aacee376bd593464be6576d2e
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-01-29 11:11:12 +0700

    handle query news

--- src/components/HackerNews.js
+++ src/components/HackerNews.js
@@ -1,70 +1,30 @@
-import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
+import { debounce } from "lodash";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getNews } from "../sagas/news/newsSlice";
+import { getNews, setQuery } from "../sagas/news/newsSlice";
 
 const HackerNews = () => {
-  // const [hits, setHits] = useState([]);
-  // const [query, setQuery] = useState("");
-  // const [loading, setLoading] = useState(true);
-  // const [errorMessage, setErrorMessage] = useState("");
-  // const handleFetchData = useRef({});
-  // const [url, setUrl] = useState(
-  //   `https://hn.algolia.com/api/v1/search?query=react`
-  // );
-
-  // const isMounted = useRef(true);
-
-  // useEffect(() => {
-  //   isMounted.current = true;
-  //   return () => {
-  //     isMounted.current = false;
-  //   };
-  // }, []);
-
-  // handleFetchData.current = async () => {
-  //   setLoading(true);
-  //   try {
-  //     const response = await axios.get(url);
-  //     setTimeout(() => {
-  //       if (isMounted.current) {
-  //         setHits(response.data?.hits || []);
-  //         setLoading(false);
-  //       }
-  //     }, 3000);
-  //   } catch (error) {
-  //     setLoading(false);
-  //     setErrorMessage(`The error happend ${error}`);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   handleFetchData.current();
-  // }, [url]);
+  const { hits, loading, errorMessage, query } = useSelector(
+    (state) => state.news
+  );
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getNews());
-  }, [dispatch]);
-  const { hits, loading, errorMessage } = useSelector((state) => state.news);
+    dispatch(getNews(query));
+  }, [dispatch, query]);
+  const handleChangeQuery = debounce((e) => {
+    dispatch(setQuery(e.target.value));
+  }, 500);
   return (
     <div className="w-2/4 p-5 mx-auto mt-5 mb-5 bg-white rounded-lg shadow-md">
-      {/* <div className="hidden mb-5 gap-x-5">
+      <div className="flex mb-5 gap-x-5">
         <input
           type="text"
-          className="block w-full p-5 transition-all border border-gray-200 rounded-md focus:border-blue-400"
+          className="block w-full p-5 transition-all border border-gray-200 rounded-md outline-none focus:border-blue-400"
           placeholder="Typing your keyword..."
           defaultValue={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={handleChangeQuery}
         />
-        <button
-          onClick={() =>
-            setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
-          }
-          className="flex-shrink-0 p-5 font-semibold text-white bg-blue-500 rounded-md"
-        >
-          Fetching
-        </button>
-      </div> */}
+      </div>
       {loading && (
         <div className="w-8 h-8 mx-auto my-10 border-4 border-r-4 border-blue-500 rounded-full loading border-r-transparent animate-spin"></div>
       )}
